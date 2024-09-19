d1dc0a03c3cbe6275efd6ee856b22856e36e3d80
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-09-19 09:27:31 +0700

    update: login page ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,24 +4,28 @@
     <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{ url('/') }}"><b>{{ config('app.name') }}</b></a>
+            <a href="#">
+                <span class="text-bold">Daily</span><span style="font-weight: 100">Operation</span>
+            </a>
         </div>
-        <div class="card">
-            <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-
+        <!-- /.login-logo -->
+        <div class="card rounded rounded-lg">
+            <div class="card-body login-card-body container-fluid">
+                <img class="text-center d-block justify-content-center py-3 mx-auto w-75"
+                     src="{{ asset('adminlte/dist/img/pakerin.svg') }}">
                 <form action="{{ route('login') }}" method="post">
                     @csrf
                     <div class="input-group mb-3">
-                        <input type="text" name="nip" class="form-control" placeholder="NIP" required>
+                        <input type="text" class="form-control" placeholder="NIP" value=""
+                               name="nip" required>
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-key"></span>
+                                <span class="fas fa-sort-numeric-down"></span>
                             </div>
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control" placeholder="Password" required>
+                        <input type="password" class="form-control" placeholder="Password" name="password" required>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -29,30 +33,22 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember" name="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
+
                         <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <div class="col-12">
+                            <button class="btn btn-primary btn-block">Sign In</button>
                         </div>
                         <!-- /.col -->
                     </div>
                 </form>
 
-                {{--            <p class="mb-1">--}}
-                {{--                <a href="{{ route('password.request') }}">I forgot my password</a>--}}
-                {{--            </p>--}}
-                <p class="mb-0">
-                    <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+                <p class="mb-1 mt-4 ">
+                    Lupa Password? Hub. Bengkel Komputer Depan
                 </p>
+
             </div>
             <!-- /.login-card-body -->
         </div>
     </div>
+
 @endsection
